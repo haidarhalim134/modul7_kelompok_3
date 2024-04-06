cd8149a08e82ea8a3c6c2818a99f924c59c86406
Haidar <haidarah134@gmail.com>
2024-04-06 08:34:28 +0700

    menambahkan class DataMahasiswa1302220152

--- modul7_kelompok_3/Program.cs
+++ modul7_kelompok_3/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using modul7_kelompok_3;
+
 Console.WriteLine("Hello, World!");
+
+DataMahasiswa1302220152 dthaidar = new DataMahasiswa1302220152();
+dthaidar.ReadJson();
